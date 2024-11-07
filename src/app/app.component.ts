503c87ac58ec361d7b521be79c334a34b7626fbc
Vertiquo <bingoriku@gmail.com>
2024-11-07 19:17:26 +0100

    Add full Clarity support by enabling animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ClarityModule } from "@clr/angular";
+import { ClarityModule } from '@clr/angular';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ClarityModule, FormsModule],
+  imports: [
+    RouterOutlet, 
+    ClarityModule, 
+    FormsModule,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
