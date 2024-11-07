e95de570bf294ae0f8810fadbac4ae8ff65deb93
Vertiquo <bingoriku@gmail.com>
2024-11-07 19:32:47 +0100

    Add Test HTML Elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FileUploadComponent } from './file-upload/file-upload.component';
+import { ClarityModule } from '@clr/angular';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FileUploadComponent, RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    ClarityModule, 
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ibsys2';
+
+  protected readonly form = new FormGroup({
+    files: new FormControl<FileList | null>(null),
+  });
 }
