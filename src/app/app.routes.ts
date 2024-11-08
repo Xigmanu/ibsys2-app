f5ea1549b02886b3ebb9e7ce3c793de4c0983a49
Vertiquo <bingoriku@gmail.com>
2024-11-08 15:33:50 +0100

    Add homepage page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { SetupComponent } from './setup/setup.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
     {
         path: 'setup',
         component: SetupComponent
+    },
+    {
+        path: '',
+        component: HomepageComponent
     }
 ];
