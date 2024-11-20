3316e15b0804150b88a8c7a0130569f9e7b3d455
Vertiquo <bingoriku@gmail.com>
2024-11-20 20:41:44 +0100

    Add preview of result data

--- src/app/export/export.component.ts
+++ src/app/export/export.component.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClarityModule } from '@clr/angular';
 import { ClarityIcons, downloadIcon } from '@cds/core/icon';
+import { DataService, SellWishItem, SellDirectItem, Production, Order, WorkingTime } from '../data.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-export',
   standalone: true,
   imports: [
-    ClarityModule
+    ClarityModule,
+    CommonModule
   ],
   templateUrl: './export.component.html',
   styleUrl: './export.component.scss'
 })
-export class ExportComponent {
+export class ExportComponent implements OnInit {
+  sellWishItems: SellWishItem[] = [];
+  sellDirectItems: SellDirectItem[] = [];
+  orderListItems: Order[] = [];
+  productionListItems: Production[] = [];
+  workingTimeListItems: WorkingTime[] = [];
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    const data = this.dataService.getData();
+    if (data) {
+      this.sellWishItems = data.output.sellWish.items;
+      this.sellDirectItems = data.output.sellDirect.items;
+      this.orderListItems = data.output.orderList.orders;
+      this.productionListItems = data.output.productionList.productions;
+      this.workingTimeListItems = data.output.workingTimeList.workingTimes;
+    } else {
+      console.error("Data is null or not available.");
+    }
+  }
 }
+
 
 ClarityIcons.addIcons(downloadIcon);
