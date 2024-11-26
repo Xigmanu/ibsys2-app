64e729bb783778e29406580555b98c6939d1c2c3
dmytro.stadnytskyi <dmytro.stadnytskyi@cas.de>
2024-11-26 14:54:45 +0100

    Add 'next' button

--- src/app/disposition/disposition.component.ts
+++ src/app/disposition/disposition.component.ts
@@ -29,17 +29,10 @@ export class DispositionComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        const foo: DispositionTableRow = {
-            article_ref_id: "22",
-            orders_in_process: this.dataStruct.input.ordersInWork[0].amount,
-            orders_prod: this.dataStruct.input.ordersInWork[0].batch,
-            orders_queued: this.dataStruct.input.waitingListStock[0].id,
-            stock_prev: this.dataStruct.input.warehouseStock[0].amount,
-            sales_req: this.dataStruct.input.futureInwardStockMovement[0].amount
-        }
-        console.log("of")
-        this.rows_p1.push(foo)
-        this.rows_p2.push(foo)
-        this.rows_p3.push(foo)
+        // TODO
+    }
+
+    onDataSafe() {
+        // TODO
     }
 }
