9152cfbcf375fb4ff95d0717d095df715549c957
dmytro.stadnytskyi <dmytro.stadnytskyi@cas.de>
2024-11-28 11:57:44 +0100

    Add more math

--- src/app/disposition/disposition-table/disposition-data-updater.ts
+++ src/app/disposition/disposition-table/disposition-data-updater.ts
@@ -2,34 +2,50 @@ import { AbstractControl, FormArray } from "@angular/forms";
 import { DispositionTableRow, DispositionTableRowName } from "./disposition-util";
 import { DataStructure } from "../../data.service";
 
-const articleComponentArr: number[][] = [
-    [1, 26, 51, 16, 17, 50, 4, 10, 49, 7, 13, 18],
-    [2, 26, 56, 16, 17, 55, 5, 11, 54, 8, 14, 19],
-    [3, 26, 31, 16, 17, 30, 6, 12, 29, 9, 15, 20],
+const articleComponentMap = [
+    [[1], [26, 51], [16, 17, 50], [4, 10, 49], [7, 13, 18]],
+    [[2], [26, 56], [16, 17, 55], [5, 11, 54], [8, 14, 19]],
+    [[3], [26, 31], [16, 17, 30], [6, 12, 29], [9, 15, 20]],
 ];
 const primaryArticleIds: number[] = [1, 2, 3];
 const commonComponentIds: number[] = [26, 16, 17];
 
 export function createTableRows(dataStruct: DataStructure, articleIdx: number): DispositionTableRow[] {
-    const articleIds: number[] = articleComponentArr.at(articleIdx);
-    if (!articleIds) {
+    const map = articleComponentMap[articleIdx]
+    if (!map) {
         return [];
     }
-
-
+    const initialRows = map.reduce((merged, arr) => merged.concat(arr)).map(id => getDataById(dataStruct, id))
+    
 }
 
-export function updateTableFormArray(row: DispositionTableRow[]) {
-    const articleIds: number[] = articleComponentArr.at(getArticleId(row));
-    if (!articleIds) {
-        return [];
+/**
+ * Reads values from the passed struct for item/order id and constructs an initial table row.
+ */
+function getDataById(struct: DataStructure, id: number): DispositionTableRow {
+    const sellWish = (id in primaryArticleIds) ? struct.output.sellWish.items.find(item => item.article == id)?.quantity ?? 0: 0;
+    const oldStock = struct.input.warehouseStock.find(stock => stock.id == id)?.amount ?? 0;
+    const queuedOrder = struct.input.waitingListWorkstations
+        .map(wlw => wlw.waitingList
+            ?.filter(vl => vl.order == id)
+            .map(order => order.amount ?? 0)
+            .reduce((sum, amount) => sum + amount, 0))
+        .map(value => value ?? 0)
+        .reduce((sum, amount) => sum + amount, 0)
+    const activeOrder = struct.input.ordersInWork
+        .filter(order => order.order == id)
+        .map(order => order.amount ?? 0)
+        .reduce((sum, amount) => sum + amount, 0);
+    
+    return {
+        [DispositionTableRowName.ARTICLE_ID]: id,
+        [DispositionTableRowName.SALES_REQUEST]: sellWish,
+        [DispositionTableRowName.STOCK_SAFETY]: 0,
+        [DispositionTableRowName.STOCK_OLD]: (id in commonComponentIds) ? oldStock / 3 : oldStock,
+        [DispositionTableRowName.ORDERS_QUEUED]: (id in commonComponentIds) ? queuedOrder * 3 : queuedOrder,
+        [DispositionTableRowName.ORDERS_ACTIVE]: (id in commonComponentIds) ? activeOrder * 3 : activeOrder,
+        [DispositionTableRowName.ORDERS_PROD]: 0
     }
-}
-
-function getArticleId(rows: DispositionTableRow[]): string | undefined {
-    return rows.find(row => row[DispositionTableRowName.ARTICLE_ID] in primaryArticleIds)?.[
-      DispositionTableRowName.ARTICLE_ID
-    ];
 }
 
 function calculateProdOrderForRow(rowControl: AbstractControl<any, any>): number {
@@ -39,5 +55,6 @@ function calculateProdOrderForRow(rowControl: AbstractControl<any, any>): number
     const orderQueued = rowControl.get(DispositionTableRowName.ORDERS_QUEUED)?.value;
     const orderActive = rowControl.get(DispositionTableRowName.ORDERS_ACTIVE)?.value;
 
-    return sellReq + stockSafety - stockOld - orderQueued - orderActive;
+    const order = sellReq + stockSafety - stockOld - orderQueued - orderActive;
+    return order < 0 ? 0 : order;
 }
