35cfded648247da7795dc0dcfb24540ae6dc212d
Dmytro Stadnytskyi <127896355+Xigmanu@users.noreply.github.com>
2024-11-30 14:20:56 +0100

    Format code

--- src/app/disposition/disposition-table/disposition-table.module.ts
+++ src/app/disposition/disposition-table/disposition-table.module.ts
@@ -1,50 +1,63 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { ClrFormsModule, ClrInputModule } from "@clr/angular";
-import { CommonModule } from "@angular/common";
-import { AbstractControl, FormArray, FormBuilder, FormGroup, ReactiveFormsModule } from "@angular/forms";
-import { TranslateModule } from "@ngx-translate/core";
-import { createFormGroupFromRow, DispositionTableRow, DispositionTableRowName, updateTableRows } from "./disposition-table-row";
+import { Component, Input, OnInit } from '@angular/core';
+import { ClrFormsModule, ClrInputModule } from '@clr/angular';
+import { CommonModule } from '@angular/common';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import {
+  createFormGroupFromRow,
+  DispositionTableRow,
+  DispositionTableRowName,
+  updateTableRows,
+} from './disposition-table-row';
 
 @Component({
-    selector: 'po-table',
-    templateUrl: './disposition-table.module.html',
-    standalone: true,
-    imports: [
-        CommonModule, 
-        ClrInputModule, 
-        ClrFormsModule, 
-        ReactiveFormsModule,
-        TranslateModule
-    ]
+  selector: 'po-table',
+  templateUrl: './disposition-table.module.html',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ClrInputModule,
+    ClrFormsModule,
+    ReactiveFormsModule,
+    TranslateModule,
+  ],
 })
 export class ProdOrdersTableModule implements OnInit {
-    @Input() dataRef: DispositionTableRow[] = [];
-    form: FormGroup;
-    rowNameEnum: typeof DispositionTableRowName = DispositionTableRowName;
+  @Input() dataRef: DispositionTableRow[] = [];
+  form: FormGroup;
+  rowNameEnum: typeof DispositionTableRowName = DispositionTableRowName;
 
-    constructor(
-        private fb: FormBuilder
-    ) {
-        this.form = this.fb.group({
-            rows: this.fb.array([])
-        });
-    }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      rows: this.fb.array([]),
+    });
+  }
 
-    get rows() {
-        return this.form.get('rows') as FormArray;
-    }
+  get rows() {
+    return this.form.get('rows') as FormArray;
+  }
 
-    ngOnInit(): void {
-        if (!this.dataRef || this.dataRef.length == 0) {
-            return;
-        }
-        this.dataRef.map(row => createFormGroupFromRow(this.fb, row)).forEach(group => this.rows.push(group))
+  ngOnInit(): void {
+    if (!this.dataRef || this.dataRef.length == 0) {
+      return;
     }
+    this.dataRef
+      .map((row) => createFormGroupFromRow(this.fb, row))
+      .forEach((group) => this.rows.push(group));
+  }
 
-    onChange(idx: number) {
-        const rowControl: AbstractControl<any, any> = this.rows.at(idx)
-        this.dataRef[idx][DispositionTableRowName.STOCK_SAFETY] = rowControl.get(DispositionTableRowName.STOCK_SAFETY)?.value;
-        updateTableRows(this.dataRef)
-        this.dataRef.forEach((ref, i) => this.rows.at(i).setValue(ref))
-    }
+  onChange(idx: number) {
+    const rowControl: AbstractControl<any, any> = this.rows.at(idx);
+    this.dataRef[idx][DispositionTableRowName.STOCK_SAFETY] = rowControl.get(
+      DispositionTableRowName.STOCK_SAFETY
+    )?.value;
+    updateTableRows(this.dataRef);
+    this.dataRef.forEach((ref, i) => this.rows.at(i).setValue(ref));
+  }
 }
--- src/app/disposition/disposition.component.ts
+++ src/app/disposition/disposition.component.ts
@@ -1,56 +1,66 @@
-import { Component, OnInit } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { ClarityModule } from '@clr/angular'
-import { ProdOrdersTableModule } from "./disposition-table/disposition-table.module";
-import { DataService, DataStructure, DispoItem } from "../data.service";
-import { TranslateModule } from "@ngx-translate/core";
-import { createTableRows, DispositionTableRow, DispositionTableRowName } from "./disposition-table/disposition-table-row";
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
+import { ProdOrdersTableModule } from './disposition-table/disposition-table.module';
+import { DataService, DataStructure, DispoItem } from '../data.service';
+import { TranslateModule } from '@ngx-translate/core';
+import {
+  createTableRows,
+  DispositionTableRow,
+  DispositionTableRowName,
+} from './disposition-table/disposition-table-row';
 
 @Component({
-    selector: 'app-prod-orders',
-    templateUrl: './disposition.component.html',
-    standalone: true,
-    imports: [
-        ClarityModule,
-        RouterModule,
-        ProdOrdersTableModule,
-        TranslateModule
-    ]
+  selector: 'app-prod-orders',
+  templateUrl: './disposition.component.html',
+  standalone: true,
+  imports: [
+    ClarityModule,
+    RouterModule,
+    ProdOrdersTableModule,
+    TranslateModule,
+  ],
 })
 export class DispositionComponent implements OnInit {
-    private dataStruct: DataStructure;
-    rows_p1: DispositionTableRow[] = [];
-    rows_p2: DispositionTableRow[] = [];
-    rows_p3: DispositionTableRow[] = [];
+  private dataStruct: DataStructure;
+  rows_p1: DispositionTableRow[] = [];
+  rows_p2: DispositionTableRow[] = [];
+  rows_p3: DispositionTableRow[] = [];
 
-    constructor(private globalDS: DataService) {
-        this.dataStruct = this.globalDS.getData();
-    }
+  constructor(private globalDS: DataService) {
+    this.dataStruct = this.globalDS.getData();
+  }
 
-    ngOnInit(): void {
-        this.rows_p1 = createTableRows(this.dataStruct, 0);
-        this.rows_p2 = createTableRows(this.dataStruct, 1);
-        this.rows_p3 = createTableRows(this.dataStruct, 2);
-    }
+  ngOnInit(): void {
+    this.rows_p1 = createTableRows(this.dataStruct, 0);
+    this.rows_p2 = createTableRows(this.dataStruct, 1);
+    this.rows_p3 = createTableRows(this.dataStruct, 2);
+  }
 
-    onDataSafe() {
-        const p1DispoItems: DispoItem[] = this.convertRowsToDispoItemArray(this.rows_p1);
-        const p2DispoItems: DispoItem[] = this.convertRowsToDispoItemArray(this.rows_p2);
-        const p3DispoItems: DispoItem[] = this.convertRowsToDispoItemArray(this.rows_p3);
-        
-        this.dataStruct.disposition.p1.push(...p1DispoItems);
-        this.dataStruct.disposition.p2.push(...p2DispoItems);
-        this.dataStruct.disposition.p3.push(...p3DispoItems);
-    }
+  onDataSafe() {
+    const p1DispoItems: DispoItem[] = this.convertRowsToDispoItemArray(
+      this.rows_p1
+    );
+    const p2DispoItems: DispoItem[] = this.convertRowsToDispoItemArray(
+      this.rows_p2
+    );
+    const p3DispoItems: DispoItem[] = this.convertRowsToDispoItemArray(
+      this.rows_p3
+    );
 
-    convertRowsToDispoItemArray(rows: DispositionTableRow[]): DispoItem[] {
-        return rows.map(row => {
-            return {
-                articleId: row[DispositionTableRowName.ARTICLE_ID].toString(),
-                safetyStock: !row[DispositionTableRowName.STOCK_SAFETY] 
-                    ? 0 
-                    : row[DispositionTableRowName.STOCK_SAFETY]
-            };
-        });
-    }
+    this.dataStruct.disposition.p1.push(...p1DispoItems);
+    this.dataStruct.disposition.p2.push(...p2DispoItems);
+    this.dataStruct.disposition.p3.push(...p3DispoItems);
+  }
+
+  convertRowsToDispoItemArray(rows: DispositionTableRow[]): DispoItem[] {
+    return rows.map((row) => {
+      return {
+        articleId: row[DispositionTableRowName.ARTICLE_ID].toString(),
+        safetyStock: !row[DispositionTableRowName.STOCK_SAFETY]
+          ? 0
+          : row[DispositionTableRowName.STOCK_SAFETY],
+      };
+    });
+  }
 }
